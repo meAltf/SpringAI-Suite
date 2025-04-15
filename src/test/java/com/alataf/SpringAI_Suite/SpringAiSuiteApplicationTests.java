58b8c77eb0b7d8bdb8f7cf2257d6f071323a6bef
meAltf <alataf.dce.cs1@gmail.com>
2025-04-16 01:08:34 +0530

    backend impl started

---
+++ src/test/java/com/alataf/SpringAI_Suite/SpringAiSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alataf.SpringAI_Suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
